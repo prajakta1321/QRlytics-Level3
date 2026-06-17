0ed4b4b905bc48b7c6220caa8eb44aaf59c38f12
prajakta1321 <91279433+prajakta1321@users.noreply.github.com>
2026-06-17 09:38:25 +0530

    Update app.py

--- code/app.py
+++ code/app.py
@@ -3,3 +3,8 @@ import streamlit as st
 
 # Import QR generator
 from qr_generator import generate_qr
+
+# Import database functions
+from database import create_table
+from database import save_qr
+from database import get_history
