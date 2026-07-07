3bed2733104f3be47da86ce2fce6d67ab1c1661a
prajakta1321 <91279433+prajakta1321@users.noreply.github.com>
2026-07-07 09:56:07 +0530

    Update database.py

--- code/database.py
+++ code/database.py
@@ -1,1 +1,25 @@
 import sqlite3
+
+def create_table():
+    """
+    Creates qr_history table if it doesn't exist.
+    """
+
+    conn = sqlite3.connect("qr_history.db")
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS qr_history (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        data TEXT NOT NULL,
+        qr_color TEXT,
+        bg_color TEXT,
+        filename TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
