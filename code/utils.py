9fe264dbc76f0fae68d8df962210541390fa9cef
prajakta1321 <91279433+prajakta1321@users.noreply.github.com>
2026-07-10 09:59:50 +0530

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -1,1 +1,13 @@
 import re
+
+def is_valid_url(url):
+    """
+    Checks if URL format is valid.
+    """
+
+    pattern = re.compile(
+        r'^(https?:\/\/)?'
+        r'([a-zA-Z0-9.-]+)\.'
+        r'([a-zA-Z]{2,6})'
+        r'(\/\S*)?$'
+    )
